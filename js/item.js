9fcf6f4f29c61632b3123cf4d2deb6e6d0540245
Nickolay Kuropatkin <nictaporuk@yandex.ru>
2015-07-28 18:49:03 +0300

    fix

--- js/item.js
+++ js/item.js
@@ -3,6 +3,11 @@ function SeatReservation(name, initialMeal) {
     var self = this;
     self.name = name;
     self.meal = ko.observable(initialMeal);
+
+    self.formattedPrice = ko.computed(function() {
+        var price = self.meal().price;
+        return price ? "$" + price.toFixed(2) : "None";
+    });
 }
 
 // Overall viewmodel for this screen, along with initial state
@@ -17,14 +22,16 @@ function ReservationsViewModel() {
     self.availableMeals = [
         { mealName: "Standard (sandwich)", price: 0 },
         { mealName: "Premium (lobster)", price: 34.95 },
-        { mealName: "Ultimate (whole zebra)", price: 290 }
+        { mealName: "Ultimate (whole zebra)", price: 290 },
+        { mealName: "Ultimate (whole zebrawqdqw)", price: 4590 }
     ];
 
     // Editable data
     self.seats = ko.observableArray([
         new SeatReservation("Steve", self.availableMeals[0]),
         new SeatReservation("Bert", self.availableMeals[1]),
-        new SeatReservation("TEST", self.availableMeals[2])
+        new SeatReservation("TEST", self.availableMeals[2]),
+        new SeatReservation("TEST2", self.availableMeals[3])
     ]);
 }
 
